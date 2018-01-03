724cd3262b5829a26f8e9be6aeaa980fd10a7f69
Andrew Davey <ajdavey8@gmail.com>
2018-01-03 14:13:38 +0000

    Add tests for double quality reduction and quality not going below zero

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,21 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
+  it("should return the name of the item", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
+  });
+
+  it("should reduce the quality by double as is past sellIn date", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 5) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(3);
+  });
+
+  it("should return not go below 0 for quality even though out of date", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 1) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
   });
 
 });
