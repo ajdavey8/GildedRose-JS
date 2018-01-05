67ae4e0ecd39b586690d7b396684157d5072f7e3
Andrew Davey <ajdavey8@gmail.com>
2018-01-05 09:10:13 +0000

    Refactored Brie and Backstage pass methods

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -62,27 +62,41 @@ class Shop {
 
   updateQuality2() {
     for (var i = 0; i < this.items.length; i++) {
-    if (this._isAgedBrie(i)) {
-      this._getAgedBrie(i)
-    } else {
-        console.log("Needs fixing")
+    if (this._isAgedBrie(this.items[i])) {
+      this._getAgedBrie(this.items[i]);
+    } else if (this._isBackstagePass(this.items[i])){
+        this._getBackstagePass(this.items[i]);
       }
     }
   return this.items;
   }
 
-  _isAgedBrie(i) {
-      return this.items[i].name === 'Aged Brie'
+  _isAgedBrie(item) {
+      return item.name === 'Aged Brie';
   }
-  _getAgedBrie(i) {
-    if (this.items[i].sellIn <= 0 && this.items[i].quality < 50) {
-      this.items[i].quality = this.items[i].quality + 2
-    } else if (this.items[i].sellIn > 0 && this.items[i].quality < 50)
-      this.items[i].quality = this.items[i].quality + 1
-      this._reduceSellInByStandard(i)
+  _getAgedBrie(item) {
+    if (item.sellIn <= 0 && item.quality < 50) {
+      item.quality += 2;
+    } else if (item.sellIn > 0 && item.quality < 50) {
+      item.quality ++;
+      item.sellIn -= 1;
+    }
   }
 
-  _reduceSellInByStandard(i) {
-  this.items[i].sellIn = this.items[i].sellIn - 1
+  _isBackstagePass(item) {
+    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  }
+
+  _getBackstagePass(item) {
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      return item.quality = 0;
+    }
+    if (item.sellIn < 10 && item.quality < 50) {
+      item.quality += 2;
+    }
+    if (item.sellIn < 5 && item.quality < 50) {
+      item.quality ++;
+    }
   }
 }
